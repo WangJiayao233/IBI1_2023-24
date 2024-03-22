93c359f628299ee5cd9b2f0bd867f21ca8bf067c
Wang Jiayao <2286701712@qq.com>
2024-03-22 12:38:31 +0800

    Edit pseudocode

--- Practical6/Average_day.py
+++ Practical6/Average_day.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import statistics as stat
 # Pseudocode:
+# this code is used to show the average time spent on different activities in a day, besed on the data given by the professor if IBI1.
 # introduce the data given
 # define variables used for input,then use these variables to create a dictionary
+# set up 6 categories and show the time of these activities in a day.
 Sleeping=8
 Classes=6
 Studying=3.5
@@ -11,20 +13,20 @@ Tv=2
 Music=1
 others=24-Sleeping-Classes-Studying-Tv-Music
 #generate a dict, 'type1': data1, 'type2': data2,......
-dict = {'Sleeping': Sleeping,'Classes':Classes,'Studying': Studying,'TV': Tv,'Music': Music,'others': others}
-print(dict)
-list(dict.values())
+dict_activities = {'Sleeping': Sleeping,'Classes':Classes,'Studying': Studying,'TV': Tv,'Music': Music,'others': others}
+print(dict_activities)
+list(dict_activities.values())
 
 lables = ["Sleeping", "Classes", "Studying", "TV", "Music","others"]
 
 time_an_average_day = [Sleeping, Classes, Studying, Tv, Music,others]
 #generate a pie chart, lead the lables and the data to the plt.pie, use autopct to show the percentage
-#So, lables = lables(the list I created), and autopct='%1.1f%%'
+#So, lables = lables(the list I created), and autopct='%1.1f%%', autopct is used to show the percentage, and to control the decimal parts.
 plt.figure()
 plt.pie(time_an_average_day, labels=lables,autopct='%1.1f%%' )
 
 plt.show()
 
 plt.clf()
-
-print(stat.mean(list(dict.values())))
+#print the mean of the data
+print(stat.mean(list(dict_activities.values())))
--- Practical6/City_size.py
+++ Practical6/City_size.py
@@ -17,21 +17,32 @@ print(china_cities)
 UK_list = ["Stirling","Edinburgh","Glasgow","London"]
 CN_list = ["Haining","Hangzhou","Beijing","Shanghai"]
 
+#Set the size of the window.
 plt.figure(figsize=(10,6))
+#use plt.subplot to create two charts in the same time
 plt.subplot(1,2,1)
 plt.bar(UK_list,uk_cities,color=['b'],width=0.5,hatch='\\') #Adjust the properties of the chart.
+#This code of line 26 & 27 was obtained from https://www.csdn.net
 for a,b,i in zip(UK_list,uk_cities,range(len(UK_list))): # zip function
     plt.text(a,b+0.03,"%.2f"%uk_cities[i],ha='center',fontsize=10) # plt.text function
+#set a title for the chart
 plt.title("Distribution of city sizes in the UK")
+#set the x and y labels
 plt.ylabel("Population(millions)")
 plt.xlabel("Cities in UK")
 
 plt.subplot(1,2,2)
 plt.bar(CN_list,china_cities,color=['r'],width=0.5,hatch='/') #Adjust the properties of the chart.
-for a,b,i in zip(CN_list,china_cities,range(len(UK_list))): # zip function
-    plt.text(a,b+0.03,"%.2f"%china_cities[i],ha='center',fontsize=10) # plt.text function
+#This code of line 38 & 40 was obtained from https://www.csdn.net
+# zip function
+for a,b,i in zip(CN_list,china_cities,range(len(UK_list))): 
+    # plt.text function
+    plt.text(a,b+0.03,"%.2f"%china_cities[i],ha='center',fontsize=10) 
+#set a title for the chart
 plt.title("Distribution of city sizes in the China")
 plt.ylabel("Population(millions)")
 plt.xlabel("Cities in China")
+#show the charts
 plt.show()
+#Clear the charts
 plt.clf()
