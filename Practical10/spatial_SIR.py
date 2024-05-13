ef6128098e2a4a9cdb007623d5726c2c88b4ed28
Wang Jiayao <2286701712@qq.com>
2024-05-13 15:26:04 +0800

    Edit spatial_SIR.py comments

--- Practical10/spatial_SIR.py
+++ Practical10/spatial_SIR.py
@@ -55,6 +55,6 @@ for j in range(TIME):
     plt.imshow(population, cmap='viridis', interpolation='nearest')
 
     fig.canvas.flush_events() 
-    plt.draw() #Line 57-58 was cited from https://blog.csdn.net/leo0308/article/details/132499565?ops_request_misc=&request_id=&biz_id=102&utm_term=imshow如何做动态图&utm_medium=distribute.pc_search_result.none-task-blog-2~all~sobaiduweb~default-3-132499565.142^v100^pc_search_result_base2&spm=1018.2226.3001.4187
+    plt.draw() #Line 55-58 was cited from https://blog.csdn.net/leo0308/article/details/132499565?ops_request_misc=&request_id=&biz_id=102&utm_term=imshow如何做动态图&utm_medium=distribute.pc_search_result.none-task-blog-2~all~sobaiduweb~default-3-132499565.142^v100^pc_search_result_base2&spm=1018.2226.3001.4187
 plt.pause(0) #Avoid closing the graph automatically, cited from https://blog.csdn.net/oMoDao1/article/details/81222465?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522171378636816800222895472%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fall.%2522%257D&request_id=171378636816800222895472&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~first_rank_ecpm_v1~rank_v31_ecpm-1-81222465-null-null.142^v100^pc_search_result_base2&utm_term=TKAgg显示图片如何不自动关闭&spm=1018.2226.3001.4187
 plt.clf()
